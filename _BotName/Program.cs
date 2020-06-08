47de957970d0e203d04ae942da7d4cbb414d1545
Philipp Hinz <phisn@outlook.de>
2020-06-08 21:29:42 +0200

    Add project files.

---
+++ _BotName/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _BotName
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+			=> new Program().MainAsync().GetAwaiter().GetResult();
+
+		public async Task MainAsync()
+		{
+		}
+	}
+}
