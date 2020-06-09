ac29bb5199a4b0484edb4478c383f30f06629ecc
Philipp Hinz <phisn@outlook.de>
2020-06-09 20:38:37 +0200

    Removed test module and added basic Casino Module and Controller

---
+++ _BotName/source/_casino/CasinoController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _BotName.source._casino
+{
+    [Serializable]
+    public class CasinoUser
+    {
+        public DateTime lastPlayTime = new DateTime();
+        public int Money = 100;
+    }
+
+    [Serializable]
+    public class CasinoContainer
+    {
+        public Dictionary<ulong, CasinoUser> casinoUsers;
+
+        public CasinoContainer()
+        {
+            casinoUsers = new Dictionary<ulong, CasinoUser>();
+        }
+    }
+
+    public class CasinoController
+    {
+        static private CasinoController instance;
+        static public CasinoController Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new CasinoController();
+
+                return instance;
+            }
+        }
+
+        private CasinoContainer casinoContainer = new CasinoContainer();
+
+        public CasinoUser GetUser(ulong userID)
+        {
+            CasinoUser casinoUser;
+
+            if (!casinoContainer.casinoUsers.TryGetValue(userID, out casinoUser))
+            {
+                casinoUser = new CasinoUser();
+                casinoContainer.casinoUsers.Add(userID, casinoUser);
+            }
+
+            return casinoUser;
+        }
+    }
+}
---
+++ _BotName/source/_casino/CasinoInfo.cs
@@ -0,0 +1,35 @@
+﻿using _BotName.source._casino;
+using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _BotName.source._casino
+{
+	public class CasinoInfo : ModuleBase<SocketCommandContext>
+	{
+		[Command("casino")]
+		public Task InfoAsync()
+		{
+			return ReplyAsync(
+				"Casino Commands:" +
+				" - info       Get Account Information");
+		}
+
+		[Command("casino info")]
+		public Task QueryAsync(IUser user = null)
+		{
+			user = user ?? Context.User;
+			CasinoUser casinoUser = CasinoController.Instance.GetUser(user.Id);
+
+			StringBuilder stringBuilder = new StringBuilder();
+			stringBuilder.AppendLine($"User: {user.Username}#{user.Discriminator}");
+			stringBuilder.AppendLine($"Money: {casinoUser.Money}");
+
+			return ReplyAsync(stringBuilder.ToString());
+		}
+	}
+}
