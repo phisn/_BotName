f3293bd0780df6ee75f13c12b40f2623111dceb8
Philipp Hinz <phisn@outlook.de>
2020-06-13 16:10:13 +0200

    Added initial server modules

---
+++ _BotName/Source/Server/ServerAdjust.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace _BotName.Source.Server
+{
+    [Group("server")]
+    class ServerAdjust : ModuleBase<SocketCommandContext>
+    {
+        [Command("shutdown")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public Task ShutdownAsync()
+        {
+            Process.Start("shutdown", "/s /t 0");
+            return ReplyAsync("System shutdown");
+        }
+
+        [Command("restart")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public Task RebootAsync()
+        {
+            Process.Start("shutdown", "/r /t 0");
+            return ReplyAsync("System shutdown");
+        }
+    }
+}
---
+++ _BotName/Source/Server/ServerInfo.cs
@@ -0,0 +1,17 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _BotName.Source.Server
+{
+    [Group("server")]
+    class ServerInfo : ModuleBase<SocketCommandContext>
+    {
+        [Command("info")]
+        public Task ClaimAsync()
+        {
+            return ReplyAsync("Currently not available");
+        }
+    }
+}
